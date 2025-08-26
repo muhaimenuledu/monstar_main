0218959c8995e4e6566b883626359401cd3ea693
Muhaimenul Islam <90051115+muhaimenuledu@users.noreply.github.com>
2025-08-26 15:50:51 +0600

    Update res_partner.py

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,9 +1,15 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    vendor_group = fields.Char(
+    vendor_group = fields.Selection(
+        selection=lambda self: self._get_vendor_groups(),
         string="Vendor Group",
-        help="Type a group name for this vendor."
+        help="Select a group name for this vendor."
     )
+
+    @api.model
+    def _get_vendor_groups(self):
+        groups = self.env['beta.mode'].search([])
+        return [(g.id, g.group) for g in groups if g.group]
